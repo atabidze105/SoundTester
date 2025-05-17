d9a4a7304b7fe121b913648ce0b26d44b1a977ce
atabidze105 <atabidze105@gmail.com>
2025-05-17 23:55:33 +0300

    third commit

--- SoundTester/ViewModels/VoiceTrackerViewModel.cs
+++ SoundTester/ViewModels/VoiceTrackerViewModel.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
 using Avalonia.Threading;
+using DynamicData;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 using NAudio.CoreAudioApi;
+using NAudio.Dsp;
 using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 using ReactiveUI;
+using SkiaSharp;
 using SoundTesting;
 using Splat;
 
@@ -17,13 +25,22 @@ public class VoiceTrackerViewModel : ViewModelBase
     private string _selectedDevice;
     private int _selectedDeviceIndex;
     private ObservableCollection<string> _devices;
-    private float _volume = -96;
+    private float _volume;
     private bool _isMonitoring = false;
     private bool _isEnabled = false;
     private WasapiCapture _wasapiCapture;
     private WasapiOut _wasapiOut;
     private BufferedWaveProvider _bufferedWaveProvider;
     private DevicesEnumerator _devicesEnumerator;
+    
+    private IEnumerable<ISeries> _seriesMagnitudes;
+    private IEnumerable<ISeries> _seriesFrequences;
+    private ObservableCollection<float> _magnitudes = new ObservableCollection<float>();
+    private ObservableCollection<float> _frequences = new ObservableCollection<float>();
+    
+    public ObservableCollection<ISeries> MySeries { get; } = new ObservableCollection<ISeries>();
+    
+    
 
     public string SelectedDevice
     {
@@ -62,9 +79,71 @@ public class VoiceTrackerViewModel : ViewModelBase
     }
 
     public ICommand RecordCommand { get; }
+
+    public IEnumerable<ISeries> SeriesMagnitudes
+    {
+        get => _seriesMagnitudes;
+        set => this.RaiseAndSetIfChanged(ref _seriesMagnitudes, value);
+    }
+    
+    public IEnumerable<ISeries> SeriesFrequences
+    {
+        get => _seriesFrequences;
+        set => this.RaiseAndSetIfChanged(ref _seriesFrequences, value);
+    }
+
+    public ObservableCollection<float> Magnitudes
+    {
+        get => _magnitudes;
+        set => this.RaiseAndSetIfChanged(ref _magnitudes, value);
+    }
+
+    public ObservableCollection<float> Frequences
+    {
+        get => _frequences;
+        set => this.RaiseAndSetIfChanged(ref _frequences, value);
+    }
+    
+    public Axis[] XAxisMagnitudes { get; set; } = new Axis[] { new Axis { Name = "Частота (Гц)" }  };
+    public Axis[] YAxisMagnitudes { get; set; } = new Axis[] { new Axis { Name = "Амплитуда" }  };
+
+    
     
     public VoiceTrackerViewModel(DevicesEnumerator? devicesEnumerator = null) //Конструктор
     {
+        SeriesMagnitudes = new ISeries[]
+        {
+            new LineSeries<float>
+            {
+                Values = Magnitudes,
+                Name = "Амплитуда",
+                Stroke = new SolidColorPaint(SKColors.LightSkyBlue, 2),
+                GeometryStroke = null,  
+                GeometryFill = null,    
+                GeometrySize = 0,
+            }
+        };
+
+        SeriesFrequences = new ISeries[]
+        {
+            new LineSeries<float>
+            {
+                Values = Frequences,
+                Name = "Частота",
+                Stroke = new SolidColorPaint(SKColors.LightSkyBlue, 2),
+                GeometryStroke = null,  
+                GeometryFill = null,    
+                GeometrySize = 0,
+            }
+        };
+        
+        MySeries.Add(new LineSeries<float>
+        {
+            Values = new ObservableCollection<float>(),
+            GeometrySize = 0,
+            LineSmoothness = 0
+        });
+        
         _devicesEnumerator = devicesEnumerator ?? Locator.Current.GetService<DevicesEnumerator>()!;
 
         Devices = _devicesEnumerator!.DevicesUpdater.InputDevices;
@@ -98,6 +177,11 @@ public class VoiceTrackerViewModel : ViewModelBase
         });
     }
 
+    private void TimerOnTick(object? sender, EventArgs e)
+    {
+        
+    }
+
     private void VoiceTrackerInit() //Инициаизация трекера
     {
         var en = new MMDeviceEnumerator(); //Костыль
@@ -105,15 +189,15 @@ public class VoiceTrackerViewModel : ViewModelBase
         var outD = en.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
         
             
-        _wasapiCapture = new WasapiCapture(inD, false, 50);
-        _wasapiOut = new WasapiOut(outD, AudioClientShareMode.Shared, false, 50);
+        _wasapiCapture = new WasapiCapture(inD, false, 100);
+        _wasapiOut = new WasapiOut(outD, AudioClientShareMode.Shared, false, 100);
                 
         _wasapiCapture.DataAvailable += OnDataAvailable;
         
         _bufferedWaveProvider = new BufferedWaveProvider(_wasapiCapture?.WaveFormat){ DiscardOnBufferOverflow = true };
         _wasapiOut.Init(_bufferedWaveProvider);
             
-        Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(-96))));
+        Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(0))));
         
         en.Dispose();
     }
@@ -124,7 +208,7 @@ public class VoiceTrackerViewModel : ViewModelBase
         {
             _wasapiCapture?.StopRecording();
             _wasapiOut.Stop();
-            Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(-96))));
+            Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(0))));
             
             RecButtonContent = "Начать проверку";
         }
@@ -143,29 +227,138 @@ public class VoiceTrackerViewModel : ViewModelBase
 
     private void OnDataAvailable(object sender, WaveInEventArgs e)
     {
-        float[] samples = new float[e.BytesRecorded / 2];
-        for (int i = 0; i < e.BytesRecorded; i += 2)
-        {
-            short sample = BitConverter.ToInt16(e.Buffer, i);
-            samples[i / 2] = sample / 32768f;
-        }
+        VolumeGaining(sender, e);
+        _bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded); //Добавление данных для мониторинга микрофона
+        
+        
+        //fft
+        FFTSeriesGaining(sender, e);
+    }
 
-        float sum = 0;
+    private void VolumeGaining(object sender, WaveInEventArgs e)
+    {
+        WasapiCapture capture = sender as WasapiCapture;
+        float max = 0;
+    
+        if (capture.WaveFormat.BitsPerSample == 16)
+        {
+            // Обработка 16-битных сэмплов
+            var buffer = new WaveBuffer(e.Buffer);
+            int samples = e.BytesRecorded / 2;
+        
+            for (int i = 0; i < samples; i++)
+            {
+                var sample = buffer.ShortBuffer[i] / 32768f;
+                if (sample < 0) sample = -sample;
+                if (sample > max) max = sample;
+            }
+        }
+        else if (capture.WaveFormat.BitsPerSample == 32 && 
+                 capture.WaveFormat.Encoding == WaveFormatEncoding.IeeeFloat)
+        {
+            // Обработка 32-битных float сэмплов
+            var buffer = new WaveBuffer(e.Buffer);
+            int samples = e.BytesRecorded / 4;
+        
+            for (int i = 0; i < samples; i++)
+            {
+                var sample = buffer.FloatBuffer[i];
+                if (sample < 0) sample = -sample;
+                if (sample > max) max = sample;
+            }
+        }
+        
+        Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(max*100)))); //Отображение значение дБ в прогрессбаре
+        
+    }
+
+    private void FFTSeriesGaining(object sender, WaveInEventArgs e)
+    { 
+        WasapiCapture wasapiCapture = sender as WasapiCapture;
+        // Конвертция сырых данных в float[]
+        float[] audioData = new float[e.BytesRecorded / 2];
+        for (int i = 0; i < audioData.Length; i++)
+        {
+            short sample = (short)(e.Buffer[i * 2 + 1] << 8 | e.Buffer[i * 2]);
+            audioData[i] = sample / 32768f; // Нормализация в [-1, 1]
+        }
+        
+        //Выбор размера FFT (степень двойки)
+        int fftSize = 1024;
+        if (audioData.Length < fftSize) return;
+        
+        //Обрезка или дополнение нулями 
+        float[] fftInput = new float[fftSize];
+        Array.Copy(audioData, fftInput, Math.Min(audioData.Length, fftSize));
+        
+        // Применение оконной функци Ханна
+        ApplyHannWindow(fftInput, fftSize);
+        
+        // Подготовка Complex[] для FFT
+        Complex[] fftBuffer = new Complex[fftSize];
+        for (int i = 0; i < fftSize; i++)
+        {
+            fftBuffer[i].X = fftInput[i]; // Реальная часть
+            fftBuffer[i].Y = 0;          // Мнимая часть = 0
+        }
+        
+        // Выполнение FFT
+        NAudio.Dsp.FastFourierTransform.FFT(true, (int)Math.Log(fftSize, 2), fftBuffer);
+        
+        // Получение амплитуды частот (для визуализации)
+        float[] magnitudes = new float[fftSize / 2];
+        for (int i = 0; i < fftSize / 2; i++)
+        {
+            magnitudes[i] = (float)Math.Sqrt(fftBuffer[i].X * fftBuffer[i].X + fftBuffer[i].Y * fftBuffer[i].Y);
+        }
+        
+        // Получение децибел
+        // Конвертация byte[] -> float[] (нормализация)
+        var samples = new float[e.BytesRecorded / 2];
         for (int i = 0; i < samples.Length; i++)
         {
-            sum += samples[i] * samples[i];
+            short sample = (short)(e.Buffer[i * 2 + 1] << 8 | e.Buffer[i * 2]);
+            samples[i] = sample / 32768f; // [-1, 1]
         }
 
-        float rms = (float)Math.Sqrt(sum / samples.Length);
+        // Усреднение амплитуд за 10 мс
+        int samplesPerPoint = 44100 * 10 / 1000;
+        var averaged = samples
+            .Select((x, i) => new { Index = i / samplesPerPoint, Value = Math.Abs(x) })
+            .GroupBy(x => x.Index)
+            .Select(g => g.Average(x => x.Value))
+            .ToArray();
 
-        float res = rms > 0 ? 20 * (float)Math.Log10(rms) : -96;
-        Dispatcher.UIThread.InvokeAsync(new Action((() => RefreshProgressBar(res)))); //Отображение значение дБ в прогрессбаре
+        // Обновление данных на UI-потоке
+        Avalonia.Threading.Dispatcher.UIThread.Post(() =>
+        {
+            var lineSeries = (LineSeries<float>)MySeries[0];
+            lineSeries.Values = new ObservableCollection<float>(averaged);
+        });
         
-        _bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded); //Добавление данных для мониторинга микрофона
+        
+        //Передача в график необходимых значений
+        SeriesMagnitudes.First().Values = magnitudes;
+        //SeriesFrequences.Last().Values = null;
+    }
+    
+    private void ApplyHannWindow(float[] data, int frameSize)
+    {
+        for (int n = 0; n < frameSize; n++)
+        {
+            double multiplier = NAudio.Dsp.FastFourierTransform.HannWindow(n, frameSize);
+            data[n] = (float)(data[n] * multiplier);
+        }
     }
 
     private void RefreshProgressBar(float percentage)
     {
         Volume = percentage;
+    }
+
+    private void UpdateCartesianChart(ObservableCollection<float> data)
+    {
+        _magnitudes.Clear();
+        _magnitudes = data;
     }
 }
--- SoundTester/obj/Debug/net9.0/SoundTester.AssemblyInfo.cs
+++ SoundTester/obj/Debug/net9.0/SoundTester.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804ce5e996eb9473233c94c8b1d6506b63fc0026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a258f7b50f0084466899457311a86a90f742e472")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
